49f43a98edf2910ab188e2842a6a7d28e14d52bc
Burak Özlece <burakozleceplus@gmail.com>
2023-03-09 17:55:53 +0300

    6 bölüme geçildi

--- bookDemo/Controllers/BooksController.cs
+++ bookDemo/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using bookDemo.Data;
 using bookDemo.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
@@ -63,9 +64,38 @@ namespace bookDemo.Controllers
             ApplicationContext.Books.Remove(entity);
 
             book.Id = entity.Id;
-            ApplicationContext.Books.Add(entity);
+            ApplicationContext.Books.Add(book);
             return Ok(book);
 
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAllBooks()
+        {
+            ApplicationContext.Books.Clear();
+            return NoContent(); 
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name ="id")] int id)
+        {
+            var entity = ApplicationContext.Books.Find(x=>x.Id.Equals(id));
+            if(entity is null) 
+                return NotFound();
+            ApplicationContext.Books.Remove(entity);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult PartialUpdateOneBook([FromRoute(Name ="id")]int id, [FromBody] JsonPatchDocument<Book> bookPath)
+        {
+            var entity = ApplicationContext.Books.Find(x => x.Id.Equals(id));
+            if (entity is null)
+            {
+                return NotFound();
+            }
+            bookPath.ApplyTo(entity);
+            return NoContent();
         }
     }
 }
